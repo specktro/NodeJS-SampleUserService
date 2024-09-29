5a82fec8bcb02ae68dde78fea92567f4f6b54513
@specktro <specktro@nonull.mx>
2024-09-29 14:00:18 -0600

    feat: Add CRUD requests for users

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const db = require('./queries')
 const app = express()
 const port = 3000
 
@@ -14,6 +15,11 @@ app.use(
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
 })
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
