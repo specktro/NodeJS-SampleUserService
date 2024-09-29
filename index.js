26860e6865161bc3c58341b3abebd0321b4ac57e
@specktro <specktro@nonull.mx>
2024-09-29 14:00:47 -0600

    eat: Add CORS middleware

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+var cors = require('cors')
 const bodyParser = require('body-parser')
 const db = require('./queries')
 const app = express()
 const port = 3000
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.use(
@@ -11,6 +13,16 @@ app.use(
         extended: true
     })
 )
+
+app.use((req, res, next) => {
+    const error = new Error('Something went wrong');
+    next(error);
+});
+// Error-handling Middleware
+app.use((err, req, res, next) => {
+    console.error('Error:', err.message);
+    res.status(500).send('Internal Server Error');
+});
 
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
